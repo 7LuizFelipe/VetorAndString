dc6591ee1a47553d26989d4db338238c5da6cbff
7LuizFelipe <163323012+7LuizFelipe@users.noreply.github.com>
2024-08-12 14:53:36 -0300

    Add files via upload

---
+++ Atvd String4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    char profissao[50];
+    double salario, novoSalario;
+
+    cout << "Digite sua profissao: ";
+    cin.getline(profissao, 50);
+
+    cout << "\nDigite seu salario: ";
+    cin >> salario;
+
+    if(strcmp(profissao, "gerente") == 0 || strcmp(profissao, "Gerente") == 0){
+        novoSalario=salario+(salario*10/100);
+    }else if(strcmp(profissao, "engenheiro")== 0 || strcmp(profissao, "Engenheiro") == 0){
+        novoSalario=salario+(salario*20/100);
+    }else if(strcmp(profissao, "tecnico") == 0 || strcmp(profissao, "Tecnico") == 0){
+        novoSalario=salario+(salario*30/100);
+    }else{
+        novoSalario=salario+(salario*5/100);
+    }
+
+    cout << "\n\nO salario antigo e de: " << salario;
+    cout << "\nO novo salario e de: " << novoSalario;
+    cout << "\nA diferenca de salario e de: " << novoSalario-salario << endl;
+
+    return 0;
+}
